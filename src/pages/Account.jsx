b994485af2d409be50f0055a77f372a21359e28d
Myron Holmes <1dequanh@gmail.com>
2025-09-15 22:47:12 -0400

    All mobile forms have space around them instead of being full screen width. Both Account and Password forms are now handling errors more efficiently.

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -25,8 +25,9 @@ const Account = () => {
   };
 
   const handleSave = async () => {
-    if (user === userData){
-      return
+    if (JSON.stringify(user) === JSON.stringify(userData)) {
+      setIsEditing(false);
+      return;
     }
     setIsEditing(false);
     setLoading(true);
@@ -47,8 +48,14 @@ const Account = () => {
       const resData = await response.json();
       if (response.ok) {
         showNotification("success", resData.message);
+        
       } else {
-        showNotification("fail", resData.message);
+        showNotification(
+          "fail",
+          resData.error === "23505"
+            ? "This Email Already Exists On A Different Account."
+            : "Sorry, There Was An Error Updating Your Account."
+        );
       }
     } catch (error) {
       console.error(error);
@@ -59,122 +66,124 @@ const Account = () => {
   };
 
   return (
-    <div className="max-w-md mx-auto bg-zinc-100 shadow-md rounded-xl p-6 mt-10">
-      {showMessage && (
-        <Notification
-          type={messageType}
-          message={message}
-          onClose={hideNotification}
-        />
-      )}
-      <h2 className="text-2xl font-semibold mb-6 text-orange-500">
-        Account Info
-      </h2>
-      {loading ? (
-        <LoadingScreen
-          message={`Loading Your Account Details...`}
-          size={"large"}
-        />
-      ) : (
-        <form className="space-y-4">
-          <div>
-            <label className="block text-sm font-medium text-gray-700">
-              First Name
-            </label>
-            <input
-              type="text"
-              name="firstName"
-              value={user.firstName}
-              onChange={handleChange}
-              disabled={!isEditing}
-              required
-              className={`text-gray-700 mt-1 block w-full rounded-lg border px-3 py-2 shadow-sm focus:ring-2 focus:ring-blue-500 ${
-                isEditing
-                  ? "border-gray-300 bg-white"
-                  : "border-gray-300 bg-gray-200 cursor-not-allowed"
-              }`}
-            />
-          </div>
+    <div className="px-4 sm:px-6 md:px-0">
+      <div className="max-w-md mx-auto bg-zinc-100 shadow-md rounded-xl p-6 mt-10">
+        {showMessage && (
+          <Notification
+            type={messageType}
+            message={message}
+            onClose={hideNotification}
+          />
+        )}
+        <h2 className="text-2xl font-semibold mb-6 text-orange-500">
+          Account Info
+        </h2>
+        {loading ? (
+          <LoadingScreen
+            message={`Loading Your Account Details...`}
+            size={"large"}
+          />
+        ) : (
+          <form className="space-y-4 m-1">
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                First Name
+              </label>
+              <input
+                type="text"
+                name="firstName"
+                value={user.firstName}
+                onChange={handleChange}
+                disabled={!isEditing}
+                required
+                className={`text-gray-700 mt-1 block w-full rounded-lg border px-3 py-2 shadow-sm focus:ring-2 focus:ring-blue-500 ${
+                  isEditing
+                    ? "border-gray-300 bg-white"
+                    : "border-gray-300 bg-gray-200 cursor-not-allowed"
+                }`}
+              />
+            </div>
 
-          <div>
-            <label className="block text-sm font-medium text-gray-700">
-              Last Name
-            </label>
-            <input
-              type="text"
-              name="lastName"
-              value={user.lastName}
-              onChange={handleChange}
-              disabled={!isEditing}
-              required
-              className={`text-gray-700 mt-1 block w-full rounded-lg border px-3 py-2 shadow-sm focus:ring-2 focus:ring-blue-500 ${
-                isEditing
-                  ? "border-gray-300 bg-white"
-                  : "border-gray-300 bg-gray-200 cursor-not-allowed"
-              }`}
-            />
-          </div>
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                Last Name
+              </label>
+              <input
+                type="text"
+                name="lastName"
+                value={user.lastName}
+                onChange={handleChange}
+                disabled={!isEditing}
+                required
+                className={`text-gray-700 mt-1 block w-full rounded-lg border px-3 py-2 shadow-sm focus:ring-2 focus:ring-blue-500 ${
+                  isEditing
+                    ? "border-gray-300 bg-white"
+                    : "border-gray-300 bg-gray-200 cursor-not-allowed"
+                }`}
+              />
+            </div>
 
-          <div>
-            <label className="block text-sm font-medium text-gray-700">
-              Email
-            </label>
-            <input
-              type="email"
-              name="email"
-              value={user.email}
-              onChange={handleChange}
-              disabled={!isEditing}
-              required
-              className={`text-gray-700 mt-1 block w-full rounded-lg border px-3 py-2 shadow-sm focus:ring-2 focus:ring-blue-500 ${
-                isEditing
-                  ? "border-gray-300 bg-white"
-                  : "border-gray-300 bg-gray-200 cursor-not-allowed"
-              }`}
-            />
-          </div>
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                value={user.email}
+                onChange={handleChange}
+                disabled={!isEditing}
+                required
+                className={`text-gray-700 mt-1 block w-full rounded-lg border px-3 py-2 shadow-sm focus:ring-2 focus:ring-blue-500 ${
+                  isEditing
+                    ? "border-gray-300 bg-white"
+                    : "border-gray-300 bg-gray-200 cursor-not-allowed"
+                }`}
+              />
+            </div>
 
-          <div className="flex justify-end gap-3 pt-4">
-            {!isEditing ? (
-              <button
-                type="button"
-                onClick={() => setIsEditing(true)}
-                className="px-4 py-2 rounded-lg bg-blue-600 text-white hover:bg-blue-700"
-              >
-                Edit
-              </button>
-            ) : (
-              <>
+            <div className="flex justify-end gap-3 pt-4">
+              {!isEditing ? (
                 <button
                   type="button"
-                  onClick={() => {
-                    setIsEditing(false);
-                    setUser(userData);
-                  }}
-                  className="px-4 py-2 rounded-lg bg-gray-200 text-gray-700 hover:bg-gray-300"
+                  onClick={() => setIsEditing(true)}
+                  className="px-4 py-2 rounded-lg bg-blue-600 text-white hover:bg-blue-700"
                 >
-                  Cancel
+                  Edit
                 </button>
-                <button
-                  type="button"
-                  onClick={handleSave}
-                  className="px-4 py-2 rounded-lg bg-green-600 text-white hover:bg-green-700"
-                >
-                  Save
-                </button>
-              </>
-            )}
-          </div>
-        </form>
-      )}
+              ) : (
+                <>
+                  <button
+                    type="button"
+                    onClick={() => {
+                      setIsEditing(false);
+                      setUser(userData);
+                    }}
+                    className="px-4 py-2 rounded-lg bg-gray-200 text-gray-700 hover:bg-gray-300"
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    type="button"
+                    onClick={handleSave}
+                    className="px-4 py-2 rounded-lg bg-green-600 text-white hover:bg-green-700"
+                  >
+                    Save
+                  </button>
+                </>
+              )}
+            </div>
+          </form>
+        )}
 
-      <div>
-        <a
-          className="text-center text-blue-600 underline"
-          href="/account/password"
-        >
-          Change Your Password
-        </a>
+        <div>
+          <a
+            className="text-center text-blue-600 underline"
+            href="/account/password"
+          >
+            Change Your Password
+          </a>
+        </div>
       </div>
     </div>
   );
--- src/pages/Password.jsx
+++ src/pages/Password.jsx
@@ -54,9 +54,11 @@ const Password = () => {
       const resData = await response.json();
 
       if (response.ok) {
+        setFormData(defaultFormData)
         showNotification("success", resData.message);
       } else {
-        showNotification("fail", resData.message);
+        console.log(response)
+        showNotification("fail", response.status === 401 ? resData.message : "There Was An Error Updating Your Password.");
       }
     } catch (error) {
       console.error(error);
@@ -67,79 +69,82 @@ const Password = () => {
   };
 
   return (
-    <div className="max-w-md mx-auto bg-zinc-100 shadow-md rounded-xl p-6 mt-10">
-      {showMessage && (
-        <Notification
-          type={messageType}
-          message={message}
-          onClose={hideNotification}
-        />
-      )}
-      <h2 className="text-2xl font-semibold mb-6 text-orange-500">
-        Change Password
-      </h2>
-      {loading ? (
-        <LoadingScreen
-          message={`Loading Your Account Details...`}
-        />
-      ) : (
-      <form className="space-y-4">
-        <div>
-          <label className="block text-sm font-medium text-gray-700">
-            Current Password
-          </label>
-          <input
-            type="text"
-            name="currentPassword"
-            value={formData.currentPassword}
-            onChange={handleChange}
-            required
-            className="text-gray-700 mt-1 block w-full rounded-lg border px-3 py-2 shadow-sm focus:ring-2 focus:ring-blue-500 border-gray-300 bg-white"
+    <div className="px-4 sm:px-6 md:px-0">
+      <div className="max-w-md mx-auto bg-zinc-100 shadow-md rounded-xl p-6 mt-10">
+        {showMessage && (
+          <Notification
+            type={messageType}
+            message={message}
+            onClose={hideNotification}
           />
-        </div>
+        )}
+        <h2 className="text-2xl font-semibold mb-6 text-orange-500">
+          Change Password
+        </h2>
+        {loading ? (
+          <LoadingScreen message={`Loading Your Account Details...`} />
+        ) : (
+          <form className="space-y-4">
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                Current Password
+              </label>
+              <input
+                type="text"
+                name="currentPassword"
+                value={formData.currentPassword}
+                onChange={handleChange}
+                required
+                className="text-gray-700 mt-1 block w-full rounded-lg border px-3 py-2 shadow-sm focus:ring-2 focus:ring-blue-500 border-gray-300 bg-white"
+              />
+            </div>
 
-        <div>
-          <label className="block text-sm font-medium text-gray-700">
-            New Password
-          </label>
-          <input
-            type="text"
-            name="newPassword"
-            value={formData.newPassword}
-            onChange={handleChange}
-            required
-            className="text-gray-700 mt-1 block w-full rounded-lg border px-3 py-2 shadow-sm focus:ring-2 focus:ring-blue-500 border-gray-300 bg-white"
-          />
-        </div>
-        <div>
-          <label className="block text-sm font-medium text-gray-700">
-            Comfirm New Password
-          </label>
-          <input
-            type="text"
-            name="newPasswordComfirmed"
-            value={formData.newPasswordComfirmed}
-            onChange={handleChange}
-            required
-            className="text-gray-700 mt-1 block w-full rounded-lg border px-3 py-2 shadow-sm focus:ring-2 focus:ring-blue-500 border-gray-300 bg-white"
-          />
-        </div>
-        <div className="flex justify-end gap-3 pt-4">
-          <button
-            type="button"
-            onClick={handleSave}
-            className="px-4 py-2 rounded-lg bg-green-600 text-white hover:bg-green-700"
-          >
-            Save
-          </button>
-        </div>
-        <div>
-          <a className="text-center text-blue-600 underline" href="/account">
-            Account Information
-          </a>
-        </div>
-      </form>
-      )}
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                New Password
+              </label>
+              <input
+                type="text"
+                name="newPassword"
+                value={formData.newPassword}
+                onChange={handleChange}
+                required
+                className="text-gray-700 mt-1 block w-full rounded-lg border px-3 py-2 shadow-sm focus:ring-2 focus:ring-blue-500 border-gray-300 bg-white"
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                Comfirm New Password
+              </label>
+              <input
+                type="text"
+                name="newPasswordComfirmed"
+                value={formData.newPasswordComfirmed}
+                onChange={handleChange}
+                required
+                className="text-gray-700 mt-1 block w-full rounded-lg border px-3 py-2 shadow-sm focus:ring-2 focus:ring-blue-500 border-gray-300 bg-white"
+              />
+            </div>
+            <div className="flex justify-end gap-3 pt-4">
+              <button
+                type="button"
+                onClick={handleSave}
+                className="px-4 py-2 rounded-lg bg-green-600 text-white hover:bg-green-700"
+              >
+                Save
+              </button>
+            </div>
+            <div>
+              <a
+                className="text-center text-blue-600 underline"
+                href="/account"
+              >
+                Account Information
+              </a>
+            </div>
+          </form>
+        )}
+      </div>
     </div>
   );
 };
